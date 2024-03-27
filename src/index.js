67a1bc6f81c81a54a2805999a1c3427d0a644e33
Biren123-ui <birenkumar9102@gmail.com>
2024-03-27 18:44:59 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
+import { app } from "./app.js";
 import dotenv from "dotenv";
 import connectDB from "./db/connection.js";
 
 dotenv.config({
-    path:"./env"
-})
+  path: "./env",
+});
 
 connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8080, () => {
+      console.log(`server listen in the port number ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Mongodb connection failed", error);
+  });
+
+app.on("error", (error) => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+});
